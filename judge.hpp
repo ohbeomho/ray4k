f003dba52aa0b58a22f82c7346379df3ec040893
ohbeomho <ohbeomho0322@gmail.com>
2024-05-26 12:42:19 +0900

    Changed hit window

--- judge.hpp
+++ judge.hpp
@@ -2,7 +2,7 @@
 #define RAY4K_JUDGE_H_
 
 enum class Judgement { GREAT = 100, GOOD = 80, BAD = 50, MISS = 0, GHOST = -1 };
-enum class JudgementHitWindow { GREAT = 60, GOOD = 110, BAD = 180, MISS = 220 };
+enum class JudgementHitWindow { GREAT = 50, GOOD = 80, BAD = 120, MISS = 160 };
 enum class Grade { S = 95, A = 90, B = 85, C = 80, D = 70, F = 0 };
 
 Judgement calcScore(int hitDiff);
